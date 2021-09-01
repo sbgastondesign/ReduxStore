518b9fab1e8d20d80e07d8ed993303dcb32c17d3
Steph G <61924023+sbgastondesign@users.noreply.github.com>
2021-09-01 14:15:37 -0400

    updated cartitem

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -14,7 +14,6 @@ const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 const Cart = () => {
   const dispatch = useDispatch();
   const state = useSelector((state) => state);
-
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT)
 
   useEffect(() => {
